f246b672c491ac8b7ceb5febdc1b97d019cbaca3
gilbertkibichii <kibichiigilbert024@gmail.com>
2023-03-27 09:54:45 +0300

    add two more project

---
+++ 7-scroll-animation/index.js
@@ -0,0 +1,22 @@
+const boxes = document.querySelectorAll('.box')
+
+
+window.addEventListener('scroll', checkboxes)
+
+checkboxes();
+
+function checkboxes(){
+    const trigerBottom = window.innerHeight/5*4;
+
+    boxes.forEach(box=>{
+        const boxTop = box.getBoundingClientRect().top;
+
+
+        if(boxTop<trigerBottom){
+            box.classList.add('show')
+        }
+        else{
+            box.classList.remove('show')
+        }
+    })
+}
